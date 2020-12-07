ea8d2320aa9253ddc4f2d1836093c89f503317ca
JF6 <jfsix@microsoft.com>
2020-12-07 08:56:03 +0100

    Ingest from dataFrame

--- azure-kusto-ingest/azure/kusto/ingest/streaming_ingest_client.py
+++ azure-kusto-ingest/azure/kusto/ingest/streaming_ingest_client.py
@@ -48,9 +48,10 @@ class KustoStreamingIngestClient:
 
         ingestion_properties.format = DataFormat.CSV
 
-        self.ingest_from_file(temp_file_path, ingestion_properties)
-
-        os.unlink(temp_file_path)
+        try:
+            self.ingest_from_file(temp_file_path, ingestion_properties)
+        finally:
+            os.unlink(temp_file_path)
 
     def ingest_from_file(self, file_descriptor: Union[FileDescriptor, str], ingestion_properties: IngestionProperties):
         """Ingest from local files.
